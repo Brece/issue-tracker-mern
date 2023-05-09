4d4730c32a5aa7c49a68a913295bc55fd86e92aa
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2023-05-09 15:28:22 +0200

    fix time format in the frontend

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,9 +1,10 @@
 import { BiPencil } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
+import { DateTime } from 'luxon';
 
 function Task(props) {
     return (
-        <div className={`status-${props.task.status.toLowerCase()} my-3 py-2 px-3 border border-secondary rounded-md`}>
+        <div className={`status-${ props.task.status.toLowerCase() } my-3 py-2 px-3 border border-secondary rounded-md`}>
             <div className='flex flex-row justify-between font-semibold'>
                 <h3 className='mb-2'>
                     { props.task.title }
@@ -13,13 +14,13 @@ function Task(props) {
             <p className='mb-1'>{ props.task.text }</p>
             <div className='flex flex-row justify-between items-center'>
                 <button className='py-1 px-3 bg-white text-primary rounded-md'>
-                    <Link to={`/tasks/${props.task._id}`}>
+                    <Link to={`/tasks/${ props.task._id }`}>
                         <BiPencil size={20} />
                     </Link>
                 </button>
                 <div className='flex flex-col items-end text-xs text-neutral-500'>
-                    <p>Create on { props.task.createdAt }</p>
-                    <p>Updated on { props.task.updatedAt }</p>
+                    <p>Create on { DateTime.fromJSDate(new Date(props.task.createdAt)).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS) }</p>
+                    <p>Updated on { DateTime.fromJSDate(new Date(props.task.updatedAt)).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS) }</p>
                 </div>
             </div>
             <p>{ props.task.assigned }</p>
